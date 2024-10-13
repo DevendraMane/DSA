5f0c5b932a84263b53b8ab988f7ebdf19c4a03db
DevendraMane <devendradineshmane@gmail.com>
2024-10-13 21:55:28 +0530

    learning how this works

--- Arrays/array_basic.java
+++ Arrays/array_basic.java
@@ -3,7 +3,7 @@ package Arrays;
 public class array_basic {
     public static void main(String[] args) {
         System.out.println("this is for array");
+        int arr[] = new int[50];
     }
 
-    int arr[] = new int[50];
 }
