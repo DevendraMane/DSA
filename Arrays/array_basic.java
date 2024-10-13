c2b61b05883316faa4be9e2504d0c1e1b25fc85a
DevendraMane <devendradineshmane@gmail.com>
2024-10-13 21:51:52 +0530

    Add array_basic.java for array basics

---
+++ Arrays/array_basic.java
@@ -0,0 +1,9 @@
+package Arrays;
+
+public class array_basic {
+    public static void main(String[] args) {
+        System.out.println("this is for array");
+    }
+
+    int arr[] = new int[50];
+}
